7adc6a273aa34ba3e670d2a8ceb66b49a3151a46
AdityaGaur7 <adgaur027@gmail.com>
2024-04-15 01:42:58 +0530

    pk

--- index.js
+++ index.js
@@ -1,18 +1,26 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
- require('dotenv').config();
+require('dotenv').config();
 const http = require('http').createServer(app);
 const PORT = process.env.PORT || 3000;
 
-
 http.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
-})
-app.use(express.static(__dirname + '/public'))
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public/ok.html');
-})
+});
 
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', (req, res) => {
+  fs.readFile(__dirname + '/public/ok.html', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send('Internal Server Error');
+      return;
+    }
+    res.send(data);
+  });
+});
 
 const io = require('socket.io')(http);
 
@@ -21,6 +29,6 @@ io.on('connection', (socket) => {
 
   socket.on('message', (msg) => {
     // console.log(msg);
-    socket.broadcast.emit('message', msg)
-  })
-})
+    socket.broadcast.emit('message', msg);
+  });
+});
