dda98fdc5792b81660bd577f1ead3bfb34adbb0f
AdityaGaur7 <adgaur027@gmail.com>
2024-04-15 01:26:36 +0530

    wassup fixed

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
-
+require('dotenv').config();
 const http = require('http').createServer(app);
 const PORT = process.env.PORT || 3000;
+
 
 http.listen(PORT, () => {
   console.log(`listening on port ${PORT}`);
